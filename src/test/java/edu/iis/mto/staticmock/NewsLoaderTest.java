609ab64ffae0f9cc86169f1a24d1b90bf6ba9b3f
198176 <adriantomczak36@gmail.com>
2018-01-22 12:35:31 +0100

    Izolacja od NewsReaderFactory

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -8,18 +8,27 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import edu.iis.mto.staticmock.reader.NewsReader;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ConfigurationLoader.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
 
 public class NewsLoaderTest {
 
     @Before
     public void setUp() {
-        Configuration configuration = new Configuration();
         mockStatic(ConfigurationLoader.class);
+        Configuration configuration = new Configuration();
         ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
         when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
         when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(configuration);
+        mockStatic(NewsReaderFactory.class);
+        NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
+        NewsReader newsReader = mock(NewsReader.class);
+        IncomingNews incomingNews = new IncomingNews();
+        String readerType = "test";
+        when(newsReader.read()).thenReturn(incomingNews);
+        when(NewsReaderFactory.getReader(readerType)).thenReturn(newsReader);
     }
 
     @Test
