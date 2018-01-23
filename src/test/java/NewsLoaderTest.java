5946712613c29560d07645f02b28ac95ea975633
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-23 18:27:10 +0100

    dodanie testu sprawdzającego działanie NewsReaderFactory Zakonczenie Zadania

--- src/test/java/NewsLoaderTest.java
+++ src/test/java/NewsLoaderTest.java
@@ -10,26 +10,24 @@ import org.powermock.reflect.Whitebox;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.times;
 import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
 
 public class NewsLoaderTest {
+
     private IncomingNews incomingNews = new IncomingNews();
     private String testReaderType = "WS";
     private IncomingInfo publicInfo = new IncomingInfo("PublicInfo", SubsciptionType.NONE);
     private IncomingInfo subInfo = new IncomingInfo("subInfo", SubsciptionType.B);
     private NewsLoader newsLoader;
 
-
-    @Before
-    public void setUp() throws Exception {
+    @Before public void setUp() throws Exception {
         newsLoader = new NewsLoader();
         mockStatic(ConfigurationLoader.class);
         ConfigurationLoader configurationMockLoader = mock(ConfigurationLoader.class);
@@ -37,8 +35,8 @@ public class NewsLoaderTest {
         incomingNews.add(subInfo);
         mockStatic(NewsReaderFactory.class);
         NewsReader newsReaderMock = new NewsReader() {
-            @Override
-            public IncomingNews read() {
+
+            @Override public IncomingNews read() {
                 return incomingNews;
             }
         };
@@ -50,33 +48,37 @@ public class NewsLoaderTest {
 
     }
 
-
-    @Test
-    public void testNewsLoaderLoadNewsCheckPublicNews() {
+    @Test public void testNewsLoaderLoadNewsCheckPublicNews() {
         PublishableNews publishableNews = newsLoader.loadNews();
         List<String> result = Whitebox.getInternalState(publishableNews, "publicContent");
         assertThat(result.size(), is(1));
         assertThat(result, not(hasItem(subInfo.getContent())));
     }
 
-    @Test
-    public void testNewsLoaderTestLoadNewsCheckSubNews(){
+    @Test public void testNewsLoaderTestLoadNewsCheckSubNews() {
         final PublishableNews publishableNews = getPublishableNewsMockWithOverrideAddForSubMethod();
         mockStatic(PublishableNews.class);
         when(PublishableNews.create()).thenReturn(publishableNews);
         PublishableNews publishable = newsLoader.loadNews();
-        List<String> result = (List<String>)Whitebox.getInternalState(publishable,"subscribentContent");
+        List<String> result = (List<String>) Whitebox.getInternalState(publishable, "subscribentContent");
         assertThat(result.size(), is(1));
-        assertThat(result.get(0),is(equalTo("subInfo")));
+        assertThat(result.get(0), is(equalTo("subInfo")));
     }
 
     private PublishableNews getPublishableNewsMockWithOverrideAddForSubMethod() {
-        return new PublishableNews(){
+        return new PublishableNews() {
+
             private final List<String> subscribentContent = new ArrayList<>();
-            @Override
-            public void addForSubscription(String content, SubsciptionType subscriptionType) {
+
+            @Override public void addForSubscription(String content, SubsciptionType subscriptionType) {
                 this.subscribentContent.add(content);
             }
         };
+    }
+
+    @Test public void testLoadNewsCheckNewsReaderFactoryCalledWithCorrectParameter() {
+        newsLoader.loadNews();
+        verifyStatic(times(1));
+        NewsReaderFactory.getReader(testReaderType);
     }
 }
