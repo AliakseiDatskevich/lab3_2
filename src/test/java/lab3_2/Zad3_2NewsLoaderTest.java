3603d0a4b110dd0215b447b7faaf76a62407c298
Strek.Andrzej <198173@edu.p.lodz.pl>
2018-01-25 18:16:36 +0100

    utworzenie klasy Zad3_2NewsLoaderTest i dodanie testu oneInfoWithNoSubscripionNeededProperlyLoadedTest

---
+++ src/test/java/lab3_2/Zad3_2NewsLoaderTest.java
@@ -0,0 +1,80 @@
+package lab3_2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.NewsReader;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class})
+
+public class Zad3_2NewsLoaderTest {
+
+    private IncomingNews incomingNews;
+    private IncomingInfo infoForAll;
+    private IncomingInfo infoForSubscribers;
+    private NewsLoader newsLoader;
+    private NewsReader newsReader;
+    private NewsReaderFactory newsReaderFactory;
+    private String readerType;
+
+    @Before
+    public void setUp() {
+        Configuration configuration = new Configuration();
+        mockStatic(ConfigurationLoader.class);
+
+        ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+        ConfigurationLoader configurationMockLoader = mock(ConfigurationLoader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+        when(configurationMockLoader.loadConfiguration()).thenReturn(configuration);
+        readerType = "typReadera";
+        infoForAll = new IncomingInfo("publicContent", SubsciptionType.NONE);
+        newsReader = mock(NewsReader.class);
+        incomingNews = new IncomingNews();
+
+        newsReaderFactory = mock(NewsReaderFactory.class);
+
+        infoForSubscribers = new IncomingInfo("ContentForSubscriptionB", SubsciptionType.B);
+
+        newsLoader = new NewsLoader();
+        Whitebox.setInternalState(configuration, "readerType", readerType);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationMockLoader);
+
+        when(ConfigurationLoader.getInstance().loadConfiguration()).thenReturn(configuration);
+        mockStatic(NewsReaderFactory.class);
+        when(newsReader.read()).thenReturn(incomingNews);
+        when(NewsReaderFactory.getReader(readerType)).thenReturn(newsReader);
+    }
+
+    @Test
+    public void oneInfoWithNoSubscripionNeededProperlyLoadedTest() {
+        incomingNews.add(infoForAll);
+        PublishableNews publishableNews = newsLoader.loadNews();
+        List<String> newsList = Whitebox.getInternalState(publishableNews, "publicContent");
+        assertEquals(newsList.size(), 1);
+        assertTrue(newsList.contains(infoForAll.getContent()));
+        assertFalse(newsList.contains(infoForSubscribers.getContent()));
+
+    }
+}
