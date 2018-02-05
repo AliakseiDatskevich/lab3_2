649b20e07cc57b14e3787d4b075e8b5811cdbfd6
Kaminski.Lukasz <198151@edu.p.lodz.pl>
2018-02-05 22:55:55 +0100

    Test loadNewsShouldReturnOnePublishableNewsWithOnePublicElement dla klasy NewsLoaderTest

--- src/test/java/staticmock/test/NewsLoaderTest.java
+++ src/test/java/staticmock/test/NewsLoaderTest.java
@@ -1,14 +1,80 @@
 package staticmock.test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
 import edu.iis.mto.staticmock.NewsReaderFactory;
 import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.reader.NewsReader;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({NewsReaderFactory.class, PublishableNews.class})
+@PrepareForTest({NewsReaderFactory.class, PublishableNews.class, ConfigurationLoader.class})
 public class NewsLoaderTest {
+
+    private PublishableNews publishableNews;
+    private NewsLoader newsLoader = new NewsLoader();
+    private ConfigurationLoader configurationLoaderMock = mock(ConfigurationLoader.class);
+    private Configuration configurationMock = mock(Configuration.class);
+    private NewsReader newsReaderMock = mock(NewsReader.class);
+    private IncomingNews incomingNewsMock = mock(IncomingNews.class);
+    private IncomingInfo incomingInfoMock = mock(IncomingInfo.class);
+    private List<IncomingInfo> listOfIncomingInfoMocks = new ArrayList();
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(NewsReaderFactory.class);
+        PowerMockito.mockStatic(PublishableNews.class);
+        PowerMockito.mockStatic(ConfigurationLoader.class);
+        publishableNews = PublishableNews.create();
+    }
+
+    @After
+    public void tearDown() {
+        listOfIncomingInfoMocks.clear();
+    }
+
+    @Test
+    public void loadNewsShouldReturnOnePublishableNewsWithOnePublicElement() {
+        int expectedOutput = 1;
+        String readerType = "File";
+        String content = "Test";
+
+        listOfIncomingInfoMocks.add(incomingInfoMock);
+
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoaderMock);
+        when(configurationLoaderMock.loadConfiguration()).thenReturn(configurationMock);
+        when(configurationMock.getReaderType()).thenReturn(readerType);
+        when(NewsReaderFactory.getReader(readerType)).thenReturn(newsReaderMock);
+        when(newsReaderMock.read()).thenReturn(incomingNewsMock);
+        when(PublishableNews.create()).thenReturn(publishableNews);
+        when(incomingNewsMock.elems()).thenReturn(listOfIncomingInfoMocks);
+        when(incomingInfoMock.requiresSubsciption()).thenReturn(false);
+        when(incomingInfoMock.getContent()).thenReturn(content);
+
+        PublishableNews actualNews = newsLoader.loadNews();
+        List<String> actualOutput = Whitebox.getInternalState(actualNews, "publicContent");
+
+        assertThat(actualOutput.size(), is(expectedOutput));
+
+    }
 
 }
